4a02c88ed2c44272e04f1895f750146c514ce82d
Chaahat <chja00001@stud.uni-saarland.de>
2022-11-12 14:28:22 +0200

    Demo version

--- Exercise_Sheet_1.r
+++ Exercise_Sheet_1.r
@@ -11,8 +11,8 @@
 ## Please write below your (and your teammates') name and matriculation number. 
 ## Name: Eric Minas
 ## Matriculation number: 2568884
-## Name:
-## Matriculation number:
+## Name: Chaahat Jain
+## Matriculation number: 7025099
 ## Name:
 ## Matriculation number:
 
@@ -32,7 +32,7 @@ getwd()
 ## b) Get help with this function.
 ?getwd
 ## c) Change your working directory to another directory.
-setwd("statR")
+# setwd("statR")
 ###############
 ### Exercise 2: Normal distribution plotting
 ###############
@@ -69,21 +69,24 @@ treesHeight <- trees$Height
 ## g) Calculate the mean and standard deviation of treesHeight and plot a normal
 ##    distribution with these parameters (NB:you should not use the same x range 
 ##    as above!)
-m <- mean(treesHeight)
-s <- sd(treesHeight) 
+avg <- mean(treesHeight)
+std <- sd(treesHeight)
 
-plot(treesHeight, dnorm(treesHeight, mean = m, sd = s))
+x <- seq(-4,4, 0.25) * std + avg
+y <- dnorm(x, mean = avg, sd = std)
+plot(x, y, type = "l")
 ## h) We observe two additional tree height values (62 and 86). What's the 
 ##    likelihood that these heights (or more extreme ones) respectively 
 ##    come from the normal distribution from g)?
-# 62 (no -1 because it is on the left edge):
-pnorm((62 - m) / s)
-# 86 (use -1 beacuse it is on the right edge):
-1 - pnorm((86 - m) / s)
+pnorm(62, avg, std)                     # from z-table, 62 or more extreme values would occur <= 1.4% of the time
+pnorm(86, avg, std, lower.tail = FALSE) # from z-table, 86 or more extreme values would occur <= 5.9% of the time
 
 ## i) What do you conclude from the p-values? (informal)
 # That either values are not very likely to happen if the results are composed 
 # as a normal distribution, but also that 86 is more likely to happen than 62.
+# Getting a 62 or more extreme value is very unlikely and this would be statistically significant. 
+# Getting an 86 or more extreme value is unlikely but would not be statistically significant.
+
 
 ## j) Use the random sampling function in R to generate 25 random samples from
 ##    the normal distribution from g), and draw a histogram based on this sample.
@@ -115,6 +118,9 @@ hist(res, breaks = 11, xlim = c(58, 94), main = "Histogram of randomly selected 
 install.packages("languageR")
 library(languageR)
 
+install.packages("dplyr")
+library(dplyr)
+
 ## b) Specifically, we will deal with the dataset 'lexdec'. 
 ##    This dataset should be available to you once you've loaded languageR.
 ##    Find out what experiment the data comes from
@@ -140,33 +146,38 @@ head(lexdec, 15)
 ##   How many observations are there for this participant, i.e. how many rows 
 ##   are in your subset?
 M2 <- subset(lexdec, lexdec$Subject == "M2")
-# There are 28 entries to the Subject M2 (see: length(M2))
+# There are 79 entries to the Subject M2 (see: length(M2))
 
 ## g) looking at the summary of M2, what can you find out about the demographic 
 ##    parameters of this participant?
 summary(M2)
-# M2 is classified as a woman
+# M2 is classified as a female
 # M2 is not a native English speaker
 
 ## h) Create a histogram (using hist()) of "RT" (logarithm of reaction time) 
 ##    for M2
 
-hist(M2$RT, main = "histogram for M2 - RT")
+hist(M2$RT, main = "histogram for M2 - RT", breaks = 11)
 ## i) Create a kernel density plot for this data using density()
+# With the histogram
 m2_d <- density(M2$RT)
 lines(m2_d, lwd = 2, col = "red")
 
+# Separate plot
+plot(density(M2$RT))
+
 ## j) What is the difference between the two?
-# The density shows the where how the values are distributed, 
-# the histogram shows the same information, but in more detail
+# The density plot is a smoother curve than the histogram. 
+# The histogram provides more accurate information regarding frequency of values. 
+# The density plot is easier to compare to a normal distribution. 
 
 ## k) Is this data likely from a normal distribution? How would you check ?
 ##    (describe in words, remember to comment out text)
 # Looking at the density, it seems not to stem from a normal distribution,
-# because the values are skewed to the left side (positively skewed), with a
-# longish tail on the right side.
+# because the values are right-skewed (positively skewed). 
 # The standard error can be used to see how reliably the data could stem from a 
 # normal distribution. 
+# We could also check by comparing the mean and median values.
 
 ## l) Looking at the graph, do you think the data is skewed? In which direction?
 # I think it is positively skewed (i.e. to the left)
@@ -185,7 +196,8 @@ lines(m2_d, lwd = 2, col = "red")
 
 ## a) What measurement scale is this data? Is it discrete or continuous? Explain
 ##    in one sentence why.
-# The data continues, because it is conceivable that there are values inbetween.
+# This is a ratio scale. 
+# This is discrete data as it is being counted. There is no value between 20 and 21.
 
 ## b) The researcher is also interested in whether story telling is related to 
 ##    their reading habits. As a proxy, she asked the children, whether they have 
@@ -193,7 +205,8 @@ lines(m2_d, lwd = 2, col = "red")
 ##    from children with library card (Y) and the remaining 13 from children 
 ##    without (N). What measurements scale does this variable have?
 lib = c(rep("Y",13),rep("N",13))
-# Distinct, as there are only two unique values. 
+# This is a ratio scale. Since it is numeric. 
+# If these are considered to be strings, then it is a nominal scale. 
 
 ## c) You will now create a dataframe of this data. Start by creating a vector 
 ##    with participant IDs. Your vector should be named 'pid', and your 
@@ -201,11 +214,11 @@ lib = c(rep("Y",13),rep("N",13))
 pid <- c(seq(from = 1, to = 26, by = 1))
 
 ## d) Next, create a vector containing all the observations. Name this vector 'obs'.
-abs <- c(18,	15,	18,	19,	23,	17,	18,	24,	17,	14,	16,	16,	17,	21,	22,	18,	
+obs <- c(18,	15,	18,	19,	23,	17,	18,	24,	17,	14,	16,	16,	17,	21,	22,	18,	
          20,	21,	20,	20, 16,	17,	17,	18,	20,	26)
 
 ## e) Create a dataframe including pid, obs and lib. Assign this to 'stories'. 
-stories <- data.frame(pid, abs, lib)
+stories <- data.frame(pid, obs, lib)
 
 ## f) Take a look at the summary of your dataframe, and at the classes of your 
 ##    columns. What class are the variable 'pid' and 'lib'?
@@ -215,7 +228,8 @@ class(stories$lib) # -> lib = character
 
 ## g) Change the class of 'pid' and 'lib' to factor. Why is factor a better
 ##     class for these variables? (answer for both separately)
-# pid should be a factor, because 
+
+# Factor is better for pid since we do not want to perform mathematical operations over pid (ex: taking the mean) 
 stories$pid <- as.factor(stories$pid)
 
 # lib should be a factor, because there are only two values and we want 
@@ -223,26 +237,25 @@ stories$pid <- as.factor(stories$pid)
 stories$lib <- as.factor(stories$lib)
 
 ## h) Create a boxplot of obs for the two lib groups
-boxplot(abs ~ lib, data = stories, xlab = "Has a library card", ylab = "# of 'and then's")
+boxplot(obs ~ lib, data = stories, xlab = "Has a library card", ylab = "# of 'and then's")
 
 ## i) Are there outliers in one of the lib groups?
-# No has one outlier (26) and Yes has two (23,24)
+# "No" has one outlier (26) and "Yes" has two (23,24)
 
 ## j) Which group shows the larger interquartile range? 
-# No has the larger quantile range
+# "No" has the larger quantile range
 
 ## k) Which one has the greater overall range?
-# No has the overall larger range (from 16 = lower end of whisker to 26 = outlier)
+stories %>% group_by(lib) %>% summarize(max = max(obs), min = min(obs), med = median(obs), avg = mean(obs))
+# Both of them have the same range (10)
 
 ## l) What is a whisker? Why is the upper whisker of group "Y" so short?
-# The lines extending from the box. This line shows the next largest/smallest 
-# value in the data set that is within the range of 1.5 times the size of the box
-# The upper whisker of the Y-group is so small because the next bigger 
-# values larger than the 75th-percentile that is within 1.5 times the IQR
+# The lines extending from the box. This line shows the largest/smallest 
+# value in the data set that is within the interquartile range.
+# The upper whisker is short because the maximum value within 1.5*IQR is closer to the third quartile compared to 
+# the IQR.
 
 ## m) Compare the median of group Y with the mean - which one is plotted in your
 ##    boxplot? Why are they different?
-# The mean is shown in the boxplot.
-# the mean is the average value, while the median is the value that is in 
-# the middle of all values, i.e. 50% of all 
-# values are < median and 50% of all values are > median
+# The box plot plots the median (50% quartile). 
+# The mean is sensitive to outliers which is why the median and mean are different.
